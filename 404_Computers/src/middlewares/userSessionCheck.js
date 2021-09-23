cc8b56ad49c3f5793a8da01b7e9d252bfc5e84ae
rodrigoquiroga <thomasitoo43@gmail.com>
2021-09-23 19:48:28 -0300

    middleware de perfil

--- 404_Computers/src/middlewares/userSessionCheck.js
+++ 404_Computers/src/middlewares/userSessionCheck.js
@@ -4,4 +4,12 @@ module.exports = (req, res, next) => {
     }else{
         res.redirect('/login')
     }
+},
+
+module.exports= (req,res,next)=>{
+  if(res.locals.user && res.locals.user.id === +req.params.id ){
+      next()
+  }else{
+      res.redirect(`/perfil/${res.locals.user.id}`)
+  }
 }
