c26ae38fc5233180283b80f083d4d7caa5bafb4a
alexqs96 <alex.rps96@gmail.com>
2021-11-27 20:58:50 -0300

    fixed history section adding a slider

--- 404_Computers/public/javascripts/slider.js
+++ 404_Computers/public/javascripts/slider.js
@@ -134,3 +134,67 @@ $(document).ready(function(){
     ]
 	});
 });
+
+/* Slider para productos */
+
+$(document).ready(function(){
+	$('.historyProductsSlider').slick({
+	slidesToShow: 1,
+	centerMode: true,
+	autoplay:true,
+	nextArrow: document.getElementById('button_Right_Slider3'),
+    prevArrow: document.getElementById('button_Left_Slider3'),
+	mobileFirst:true,
+	responsive: [
+        {
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 6,
+                slidesToScroll: 6,
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 5,
+            }
+        },
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 380,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }
+    ]
+	});
+});
